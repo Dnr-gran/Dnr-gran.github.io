0783c0e44d613ef34cbf0bc336bc57cdcfe9b099
Dnr-gran <dnrgranchelli@gmail.com>
2025-03-04 19:05:19 +0100

    modified CV download link (FIX)

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,7 @@
 const downloadBtn = document.getElementById("downloadBtn").addEventListener("click", function(){
     const link = document.createElement("a");
     link.href = "Dnr-gran/Dnr-gran.github.io/assets/resume/CV- Daniel Roberto Granchelli.pdf";
-    link.dowload = "CV- Daniel Roberto Granchelli.pdf";
+    link.download = "CV- Daniel Roberto Granchelli.pdf";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
